1219fd43c9cdf9d031c75a2b55684c6695698efb
Dustin Mowcomber <dustin.mowcomber@sendgrid.com>
2020-04-14 16:29:08 -0700

    move cli stuff to the cli package

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"time"
 
 	"github.com/dmowcomber/dcc-go/throttle"
-	"github.com/eiannone/keyboard"
+	"github.com/dmowcomber/dcc-go/ui/cli"
 	"github.com/tarm/serial"
 )
 
@@ -17,21 +17,21 @@ const (
 
 func main() {
 	var address int
-	flag.IntVar(&address, "address", defaultAddress, "The address of the locomotive")
 	var device string
+	flag.IntVar(&address, "address", defaultAddress, "The address of the locomotive")
 	flag.StringVar(&device, "device", defaultDevice, "The usb device of your DCC system")
 	flag.Parse()
 
 	log.Printf("connecting to %s\n", device)
 	log.Printf("locomotive address %d\n", address)
 	serialConfig := &serial.Config{Name: device, Baud: 115200}
-	port, err := serial.OpenPort(serialConfig)
+	serialWriter, err := serial.OpenPort(serialConfig)
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Println("connected")
 
-	throttle := throttle.New(address, port)
+	throttle := throttle.New(address, serialWriter)
 	defer func() {
 		log.Println("powering off")
 		time.Sleep(300 * time.Millisecond)
@@ -39,65 +39,7 @@ func main() {
 		throttle.PowerOff()
 	}()
 
-	userInput(throttle)
-}
-
-func userInput(t *throttle.Throttle) error {
-	// TODO: print usage
-
-	err := keyboard.Open()
-	if err != nil {
-		return err
-	}
-	defer keyboard.Close()
-
-	for {
-		char, key, err := keyboard.GetKey()
-		if err != nil {
-			return err
-		}
-
-		log.Printf("%q, %#v\n", char, key)
-
-		switch char {
-		case 'p':
-			t.PowerToggle()
-		case 's':
-			t.Stop()
-		case '0':
-			t.ToggleFunction(0)
-		case '1':
-			t.ToggleFunction(1)
-		case '2', 'h':
-			t.ToggleFunction(2)
-		case '3':
-			t.ToggleFunction(3)
-		case '4':
-			t.ToggleFunction(4)
-		case '5':
-			t.ToggleFunction(5)
-		case '6':
-			t.ToggleFunction(6)
-		case '7':
-			t.ToggleFunction(7)
-		case '8', 'm':
-			t.ToggleFunction(8)
-		case '9':
-			t.ToggleFunction(9)
-		default:
-			if key == keyboard.KeyArrowUp {
-				t.ThrottleUp()
-			} else if key == keyboard.KeyArrowDown {
-				t.ThrottleDown()
-			} else if key == keyboard.KeyArrowRight {
-				t.DirectionForward()
-			} else if key == keyboard.KeyArrowLeft {
-				t.DirectionBackward()
-			} else if key == keyboard.KeyCtrlC {
-				return nil
-			} else {
-				// TODO: print usage
-			}
-		}
-	}
+	// run the cli
+	throttleCLI := cli.New(throttle)
+	throttleCLI.Run()
 }
---
+++ ui/cli/cli.go
@@ -0,0 +1,78 @@
+package cli
+
+import (
+	"log"
+
+	"github.com/dmowcomber/dcc-go/throttle"
+	"github.com/eiannone/keyboard"
+)
+
+func New(throt *throttle.Throttle) *CLI {
+	return &CLI{
+		throt: throt,
+	}
+}
+
+type CLI struct {
+	throt *throttle.Throttle
+}
+
+func (c *CLI) Run() error {
+	// TODO: print usage
+
+	err := keyboard.Open()
+	if err != nil {
+		return err
+	}
+	defer keyboard.Close()
+
+	for {
+		char, key, err := keyboard.GetKey()
+		if err != nil {
+			return err
+		}
+
+		log.Printf("%q, %#v\n", char, key)
+
+		switch char {
+		case 'p':
+			c.throt.PowerToggle()
+		case 's':
+			c.throt.Stop()
+		case '0':
+			c.throt.ToggleFunction(0)
+		case '1':
+			c.throt.ToggleFunction(1)
+		case '2', 'h':
+			c.throt.ToggleFunction(2)
+		case '3':
+			c.throt.ToggleFunction(3)
+		case '4':
+			c.throt.ToggleFunction(4)
+		case '5':
+			c.throt.ToggleFunction(5)
+		case '6':
+			c.throt.ToggleFunction(6)
+		case '7':
+			c.throt.ToggleFunction(7)
+		case '8', 'm':
+			c.throt.ToggleFunction(8)
+		case '9':
+			c.throt.ToggleFunction(9)
+		default:
+			if key == keyboard.KeyArrowUp {
+				c.throt.ThrottleUp()
+			} else if key == keyboard.KeyArrowDown {
+				c.throt.ThrottleDown()
+			} else if key == keyboard.KeyArrowRight {
+				c.throt.DirectionForward()
+			} else if key == keyboard.KeyArrowLeft {
+				c.throt.DirectionBackward()
+			} else if key == keyboard.KeyCtrlC {
+				return nil
+			} else {
+				// TODO: print usage
+			}
+		}
+	}
+}
